53755dd573f15661673162ad98f4ebd7e8396e72
ali-samer <samerali.walid@gmail.com>
2025-11-21 22:38:31 -0700

    Added imnodes git submodule and pulled updated version of aiecad_compiler from aiecad-compiler branch

--- aiecad/aiecad_compiler/examples/applications/add_activate2/generated_add_activate.py
+++ aiecad/aiecad_compiler/examples/applications/add_activate2/generated_add_activate.py
@@ -19,41 +19,44 @@ from aie.helpers.taplib import TensorAccessPattern
 @iron.jit(is_placed=False)
 def base_aaa(inputA, inputB, outputD):
     # Define tensor types
+    data_ty = np.ndarray[(inputA.numel(),), np.dtype[bfloat16]]
+    chunk_ty = np.ndarray[(inputA.numel() // 4,), np.dtype[bfloat16]]
+    worker_chunk_ty = np.ndarray[(inputA.numel() // 8,), np.dtype[bfloat16]]
     data_a_ty = np.ndarray[(inputA.numel(),), np.dtype[bfloat16]]
-    data_b_ty = np.ndarray[(inputB.numel(),), np.dtype[bfloat16]]
-    data_d_ty = np.ndarray[(outputD.numel(),), np.dtype[bfloat16]]
     chunk_a = np.ndarray[(inputA.numel() // 4,), np.dtype[bfloat16]]
-    chunk_b = np.ndarray[(inputB.numel() // 4,), np.dtype[bfloat16]]
-    chunk_d = np.ndarray[(outputD.numel() // 4,), np.dtype[bfloat16]]
     chunk_a_worker = np.ndarray[(inputA.numel() // 8,), np.dtype[bfloat16]]
+    data_b_ty = np.ndarray[(inputB.numel(),), np.dtype[bfloat16]]
+    chunk_b = np.ndarray[(inputB.numel() // 4,), np.dtype[bfloat16]]
     chunk_b_worker = np.ndarray[(inputB.numel() // 8,), np.dtype[bfloat16]]
+    data_d_ty = np.ndarray[(outputD.numel(),), np.dtype[bfloat16]]
+    chunk_d = np.ndarray[(outputD.numel() // 4,), np.dtype[bfloat16]]
     chunk_d_worker = np.ndarray[(outputD.numel() // 8,), np.dtype[bfloat16]]
 
     # Data movement with ObjectFifos
     SHIM_L3_L2_A1A2_col0 = ObjectFifo(obj_type=chunk_a, depth=2, name="SHIM_L3_L2_A1A2_col0")
-    SHIM_L3_L2_B1B2_col0 = ObjectFifo(obj_type=chunk_b, depth=2, name="SHIM_L3_L2_B1B2_col0")
     SHIM_L3_L2_A3A4_col1 = ObjectFifo(obj_type=chunk_a, depth=2, name="SHIM_L3_L2_A3A4_col1")
-    SHIM_L3_L2_B3B4_col1 = ObjectFifo(obj_type=chunk_b, depth=2, name="SHIM_L3_L2_B3B4_col1")
     SHIM_L3_L2_A5A6_col2 = ObjectFifo(obj_type=chunk_a, depth=2, name="SHIM_L3_L2_A5A6_col2")
-    SHIM_L3_L2_B5B6_col2 = ObjectFifo(obj_type=chunk_b, depth=2, name="SHIM_L3_L2_B5B6_col2")
     SHIM_L3_L2_A7A8_col3 = ObjectFifo(obj_type=chunk_a, depth=2, name="SHIM_L3_L2_A7A8_col3")
+    SHIM_L3_L2_B1B2_col0 = ObjectFifo(obj_type=chunk_b, depth=2, name="SHIM_L3_L2_B1B2_col0")
+    SHIM_L3_L2_B3B4_col1 = ObjectFifo(obj_type=chunk_b, depth=2, name="SHIM_L3_L2_B3B4_col1")
+    SHIM_L3_L2_B5B6_col2 = ObjectFifo(obj_type=chunk_b, depth=2, name="SHIM_L3_L2_B5B6_col2")
     SHIM_L3_L2_B7B8_col3 = ObjectFifo(obj_type=chunk_b, depth=2, name="SHIM_L3_L2_B7B8_col3")
     MEM_L2_L1_A1A2_col0 = SHIM_L3_L2_A1A2_col0.cons().split(obj_types=[chunk_a_worker, chunk_a_worker], offsets=[((inputA.numel() // 8) * 0), ((inputA.numel() // 8) * 1)], names=["MEM_L2_L1_A1_col0", "MEM_L2_L1_A2_col0"], placement=Tile(0, 1))
-    MEM_L2_L1_B1B2_col0 = SHIM_L3_L2_B1B2_col0.cons().split(obj_types=[chunk_b_worker, chunk_b_worker], offsets=[((inputB.numel() // 8) * 0), ((inputB.numel() // 8) * 1)], names=["MEM_L2_L1_B1_col0", "MEM_L2_L1_B2_col0"], placement=Tile(0, 1))
     MEM_L2_L1_A3A4_col1 = SHIM_L3_L2_A3A4_col1.cons().split(obj_types=[chunk_a_worker, chunk_a_worker], offsets=[((inputA.numel() // 8) * 0), ((inputA.numel() // 8) * 1)], names=["MEM_L2_L1_A3_col1", "MEM_L2_L1_A4_col1"], placement=Tile(1, 1))
-    MEM_L2_L1_B3B4_col1 = SHIM_L3_L2_B3B4_col1.cons().split(obj_types=[chunk_b_worker, chunk_b_worker], offsets=[((inputB.numel() // 8) * 0), ((inputB.numel() // 8) * 1)], names=["MEM_L2_L1_B3_col1", "MEM_L2_L1_B4_col1"], placement=Tile(1, 1))
     MEM_L2_L1_A5A6_col2 = SHIM_L3_L2_A5A6_col2.cons().split(obj_types=[chunk_a_worker, chunk_a_worker], offsets=[((inputA.numel() // 8) * 0), ((inputA.numel() // 8) * 1)], names=["MEM_L2_L1_A5_col2", "MEM_L2_L1_A6_col2"], placement=Tile(2, 1))
-    MEM_L2_L1_B5B6_col2 = SHIM_L3_L2_B5B6_col2.cons().split(obj_types=[chunk_b_worker, chunk_b_worker], offsets=[((inputB.numel() // 8) * 0), ((inputB.numel() // 8) * 1)], names=["MEM_L2_L1_B5_col2", "MEM_L2_L1_B6_col2"], placement=Tile(2, 1))
     MEM_L2_L1_A7A8_col3 = SHIM_L3_L2_A7A8_col3.cons().split(obj_types=[chunk_a_worker, chunk_a_worker], offsets=[((inputA.numel() // 8) * 0), ((inputA.numel() // 8) * 1)], names=["MEM_L2_L1_A7_col3", "MEM_L2_L1_A8_col3"], placement=Tile(3, 1))
+    MEM_L2_L1_B1B2_col0 = SHIM_L3_L2_B1B2_col0.cons().split(obj_types=[chunk_b_worker, chunk_b_worker], offsets=[((inputB.numel() // 8) * 0), ((inputB.numel() // 8) * 1)], names=["MEM_L2_L1_B1_col0", "MEM_L2_L1_B2_col0"], placement=Tile(0, 1))
+    MEM_L2_L1_B3B4_col1 = SHIM_L3_L2_B3B4_col1.cons().split(obj_types=[chunk_b_worker, chunk_b_worker], offsets=[((inputB.numel() // 8) * 0), ((inputB.numel() // 8) * 1)], names=["MEM_L2_L1_B3_col1", "MEM_L2_L1_B4_col1"], placement=Tile(1, 1))
+    MEM_L2_L1_B5B6_col2 = SHIM_L3_L2_B5B6_col2.cons().split(obj_types=[chunk_b_worker, chunk_b_worker], offsets=[((inputB.numel() // 8) * 0), ((inputB.numel() // 8) * 1)], names=["MEM_L2_L1_B5_col2", "MEM_L2_L1_B6_col2"], placement=Tile(2, 1))
     MEM_L2_L1_B7B8_col3 = SHIM_L3_L2_B7B8_col3.cons().split(obj_types=[chunk_b_worker, chunk_b_worker], offsets=[((inputB.numel() // 8) * 0), ((inputB.numel() // 8) * 1)], names=["MEM_L2_L1_B7_col3", "MEM_L2_L1_B8_col3"], placement=Tile(3, 1))
-    L1_L1_elwiseadd_relu_1 = ObjectFifo(obj_type=chunk_d_worker, depth=2, name="L1_L1_elwiseadd_relu_1")
-    L1_L1_elwiseadd_relu_2 = ObjectFifo(obj_type=chunk_d_worker, depth=2, name="L1_L1_elwiseadd_relu_2")
-    L1_L1_elwiseadd_relu_3 = ObjectFifo(obj_type=chunk_d_worker, depth=2, name="L1_L1_elwiseadd_relu_3")
-    L1_L1_elwiseadd_relu_4 = ObjectFifo(obj_type=chunk_d_worker, depth=2, name="L1_L1_elwiseadd_relu_4")
-    L1_L1_elwiseadd_relu_5 = ObjectFifo(obj_type=chunk_d_worker, depth=2, name="L1_L1_elwiseadd_relu_5")
-    L1_L1_elwiseadd_relu_6 = ObjectFifo(obj_type=chunk_d_worker, depth=2, name="L1_L1_elwiseadd_relu_6")
-    L1_L1_elwiseadd_relu_7 = ObjectFifo(obj_type=chunk_d_worker, depth=2, name="L1_L1_elwiseadd_relu_7")
-    L1_L1_elwiseadd_relu_8 = ObjectFifo(obj_type=chunk_d_worker, depth=2, name="L1_L1_elwiseadd_relu_8")
+    L1_L1_add_to_relu_1 = ObjectFifo(obj_type=worker_chunk_ty, depth=2, name="L1_L1_add_to_relu_1")
+    L1_L1_add_to_relu_2 = ObjectFifo(obj_type=worker_chunk_ty, depth=2, name="L1_L1_add_to_relu_2")
+    L1_L1_add_to_relu_3 = ObjectFifo(obj_type=worker_chunk_ty, depth=2, name="L1_L1_add_to_relu_3")
+    L1_L1_add_to_relu_4 = ObjectFifo(obj_type=worker_chunk_ty, depth=2, name="L1_L1_add_to_relu_4")
+    L1_L1_add_to_relu_5 = ObjectFifo(obj_type=worker_chunk_ty, depth=2, name="L1_L1_add_to_relu_5")
+    L1_L1_add_to_relu_6 = ObjectFifo(obj_type=worker_chunk_ty, depth=2, name="L1_L1_add_to_relu_6")
+    L1_L1_add_to_relu_7 = ObjectFifo(obj_type=worker_chunk_ty, depth=2, name="L1_L1_add_to_relu_7")
+    L1_L1_add_to_relu_8 = ObjectFifo(obj_type=worker_chunk_ty, depth=2, name="L1_L1_add_to_relu_8")
     SHIM_L2_L3_D1D2_col0 = ObjectFifo(obj_type=chunk_d, depth=2, name="SHIM_L2_L3_D1D2_col0")
     SHIM_L2_L3_D3D4_col1 = ObjectFifo(obj_type=chunk_d, depth=2, name="SHIM_L2_L3_D3D4_col1")
     SHIM_L2_L3_D5D6_col2 = ObjectFifo(obj_type=chunk_d, depth=2, name="SHIM_L2_L3_D5D6_col2")
@@ -64,55 +67,55 @@ def base_aaa(inputA, inputB, outputD):
     MEM_L1_L2_D7D8_col3 = SHIM_L2_L3_D7D8_col3.prod().join(obj_types=[chunk_d_worker, chunk_d_worker], names=["MEM_L1_L2_D7_col3", "MEM_L1_L2_D8_col3"], placement=Tile(3, 1), offsets=[((outputD.numel() // 8) * 0), ((outputD.numel() // 8) * 1)])
 
     #Define kernels here... ------------------------------------------------\/
-    element_wise_add = ExternalFunction(
-        name="eltwise_add_bf16_scalar", source_file="../../../aie_kernels/aie2/add.cc", arg_types=[chunk_a_worker, chunk_b_worker, chunk_d_worker], include_dirs=["/scratch/andrewa/mlir-aie/aie_kernels/"]
+    externalfunc1 = ExternalFunction(
+        name="eltwise_add_bf16_scalar", source_file="../../../aie_kernels/aie2/add.cc", arg_types=[worker_chunk_ty, worker_chunk_ty, worker_chunk_ty], include_dirs=["/scratch/andrewa/mlir-aie/aie_kernels/"]
     )
 
-    relu_activation = ExternalFunction(
-        name="bf16_relu", source_file="../../../aie_kernels/aie2/relu.cc", arg_types=[chunk_d_worker, chunk_d_worker], include_dirs=["/scratch/andrewa/mlir-aie/aie_kernels/"]
+    externalfunc2 = ExternalFunction(
+        name="bf16_relu", source_file="../../../aie_kernels/aie2/relu.cc", arg_types=[worker_chunk_ty, worker_chunk_ty], include_dirs=["/scratch/andrewa/mlir-aie/aie_kernels/"]
     )
 
     # core_fn here:
-    def eltwise_add(element_wise_add, inputA, inputB, outputC):
+    def corefunc1(kernel, inputA, inputB, outputC):
             elementA = inputA.acquire(1)
             elementB = inputB.acquire(1)
             elementC = outputC.acquire(1)
-            element_wise_add(elementA, elementB, elementC)
+            kernel(elementA, elementB, elementC)
             inputA.release(1)
             inputB.release(1)
             outputC.release(1)
 
-    def relu(relu_activation, inputC, outputD):
+    def corefunc2(kernel, inputC, outputD):
             elementC = inputC.acquire(1)
             elementD = outputD.acquire(1)
-            relu_activation(elementC, elementD)
+            kernel(elementC, elementD)
             inputC.release(1)
             outputD.release(1)
 
     #Workers defined here:
     Workers = []
-    A1_B1_worker = Worker(core_fn=eltwise_add, fn_args=[element_wise_add, MEM_L2_L1_A1A2_col0[0].cons(), MEM_L2_L1_B1B2_col0[0].cons(), L1_L1_elwiseadd_relu_1.prod()], placement=Tile(0, 5))
-    A2_B2_worker = Worker(core_fn=eltwise_add, fn_args=[element_wise_add, MEM_L2_L1_A1A2_col0[1].cons(), MEM_L2_L1_B1B2_col0[1].cons(), L1_L1_elwiseadd_relu_2.prod()], placement=Tile(0, 3))
-    A3_B3_worker = Worker(core_fn=eltwise_add, fn_args=[element_wise_add, MEM_L2_L1_A3A4_col1[0].cons(), MEM_L2_L1_B3B4_col1[0].cons(), L1_L1_elwiseadd_relu_3.prod()], placement=Tile(1, 5))
-    A4_B4_worker = Worker(core_fn=eltwise_add, fn_args=[element_wise_add, MEM_L2_L1_A3A4_col1[1].cons(), MEM_L2_L1_B3B4_col1[1].cons(), L1_L1_elwiseadd_relu_4.prod()], placement=Tile(1, 3))
-    A5_B5_worker = Worker(core_fn=eltwise_add, fn_args=[element_wise_add, MEM_L2_L1_A5A6_col2[0].cons(), MEM_L2_L1_B5B6_col2[0].cons(), L1_L1_elwiseadd_relu_5.prod()], placement=Tile(2, 5))
-    A6_B6_worker = Worker(core_fn=eltwise_add, fn_args=[element_wise_add, MEM_L2_L1_A5A6_col2[1].cons(), MEM_L2_L1_B5B6_col2[1].cons(), L1_L1_elwiseadd_relu_6.prod()], placement=Tile(2, 3))
-    A7_B7_worker = Worker(core_fn=eltwise_add, fn_args=[element_wise_add, MEM_L2_L1_A7A8_col3[0].cons(), MEM_L2_L1_B7B8_col3[0].cons(), L1_L1_elwiseadd_relu_7.prod()], placement=Tile(3, 5))
-    A8_B8_worker = Worker(core_fn=eltwise_add, fn_args=[element_wise_add, MEM_L2_L1_A7A8_col3[1].cons(), MEM_L2_L1_B7B8_col3[1].cons(), L1_L1_elwiseadd_relu_8.prod()], placement=Tile(3, 3))
-    C1_worker = Worker(core_fn=relu, fn_args=[relu_activation, L1_L1_elwiseadd_relu_1.cons(), MEM_L1_L2_D1D2_col0[0].prod()], placement=Tile(0, 4))
-    C2_worker = Worker(core_fn=relu, fn_args=[relu_activation, L1_L1_elwiseadd_relu_2.cons(), MEM_L1_L2_D1D2_col0[1].prod()], placement=Tile(0, 2))
-    C3_worker = Worker(core_fn=relu, fn_args=[relu_activation, L1_L1_elwiseadd_relu_3.cons(), MEM_L1_L2_D3D4_col1[0].prod()], placement=Tile(1, 4))
-    C4_worker = Worker(core_fn=relu, fn_args=[relu_activation, L1_L1_elwiseadd_relu_4.cons(), MEM_L1_L2_D3D4_col1[1].prod()], placement=Tile(1, 2))
-    C5_worker = Worker(core_fn=relu, fn_args=[relu_activation, L1_L1_elwiseadd_relu_5.cons(), MEM_L1_L2_D5D6_col2[0].prod()], placement=Tile(2, 4))
-    C6_worker = Worker(core_fn=relu, fn_args=[relu_activation, L1_L1_elwiseadd_relu_6.cons(), MEM_L1_L2_D5D6_col2[1].prod()], placement=Tile(2, 2))
-    C7_worker = Worker(core_fn=relu, fn_args=[relu_activation, L1_L1_elwiseadd_relu_7.cons(), MEM_L1_L2_D7D8_col3[0].prod()], placement=Tile(3, 4))
-    C8_worker = Worker(core_fn=relu, fn_args=[relu_activation, L1_L1_elwiseadd_relu_8.cons(), MEM_L1_L2_D7D8_col3[1].prod()], placement=Tile(3, 2))
+    worker_add_col0_w0 = Worker(core_fn=corefunc1, fn_args=[externalfunc1, MEM_L2_L1_A1A2_col0[0].cons(), MEM_L2_L1_B1B2_col0[0].cons(), L1_L1_add_to_relu_1.prod()], placement=Tile(0, 5))
+    worker_add_col0_w1 = Worker(core_fn=corefunc1, fn_args=[externalfunc1, MEM_L2_L1_A1A2_col0[1].cons(), MEM_L2_L1_B1B2_col0[1].cons(), L1_L1_add_to_relu_2.prod()], placement=Tile(0, 3))
+    worker_add_col1_w0 = Worker(core_fn=corefunc1, fn_args=[externalfunc1, MEM_L2_L1_A3A4_col1[0].cons(), MEM_L2_L1_B3B4_col1[0].cons(), L1_L1_add_to_relu_3.prod()], placement=Tile(1, 5))
+    worker_add_col1_w1 = Worker(core_fn=corefunc1, fn_args=[externalfunc1, MEM_L2_L1_A3A4_col1[1].cons(), MEM_L2_L1_B3B4_col1[1].cons(), L1_L1_add_to_relu_4.prod()], placement=Tile(1, 3))
+    worker_add_col2_w0 = Worker(core_fn=corefunc1, fn_args=[externalfunc1, MEM_L2_L1_A5A6_col2[0].cons(), MEM_L2_L1_B5B6_col2[0].cons(), L1_L1_add_to_relu_5.prod()], placement=Tile(2, 5))
+    worker_add_col2_w1 = Worker(core_fn=corefunc1, fn_args=[externalfunc1, MEM_L2_L1_A5A6_col2[1].cons(), MEM_L2_L1_B5B6_col2[1].cons(), L1_L1_add_to_relu_6.prod()], placement=Tile(2, 3))
+    worker_add_col3_w0 = Worker(core_fn=corefunc1, fn_args=[externalfunc1, MEM_L2_L1_A7A8_col3[0].cons(), MEM_L2_L1_B7B8_col3[0].cons(), L1_L1_add_to_relu_7.prod()], placement=Tile(3, 5))
+    worker_add_col3_w1 = Worker(core_fn=corefunc1, fn_args=[externalfunc1, MEM_L2_L1_A7A8_col3[1].cons(), MEM_L2_L1_B7B8_col3[1].cons(), L1_L1_add_to_relu_8.prod()], placement=Tile(3, 3))
+    worker_relu_col0_w0 = Worker(core_fn=corefunc2, fn_args=[externalfunc2, L1_L1_add_to_relu_1.cons(), MEM_L1_L2_D1D2_col0[0].prod()], placement=Tile(0, 4))
+    worker_relu_col0_w1 = Worker(core_fn=corefunc2, fn_args=[externalfunc2, L1_L1_add_to_relu_2.cons(), MEM_L1_L2_D1D2_col0[1].prod()], placement=Tile(0, 2))
+    worker_relu_col1_w0 = Worker(core_fn=corefunc2, fn_args=[externalfunc2, L1_L1_add_to_relu_3.cons(), MEM_L1_L2_D3D4_col1[0].prod()], placement=Tile(1, 4))
+    worker_relu_col1_w1 = Worker(core_fn=corefunc2, fn_args=[externalfunc2, L1_L1_add_to_relu_4.cons(), MEM_L1_L2_D3D4_col1[1].prod()], placement=Tile(1, 2))
+    worker_relu_col2_w0 = Worker(core_fn=corefunc2, fn_args=[externalfunc2, L1_L1_add_to_relu_5.cons(), MEM_L1_L2_D5D6_col2[0].prod()], placement=Tile(2, 4))
+    worker_relu_col2_w1 = Worker(core_fn=corefunc2, fn_args=[externalfunc2, L1_L1_add_to_relu_6.cons(), MEM_L1_L2_D5D6_col2[1].prod()], placement=Tile(2, 2))
+    worker_relu_col3_w0 = Worker(core_fn=corefunc2, fn_args=[externalfunc2, L1_L1_add_to_relu_7.cons(), MEM_L1_L2_D7D8_col3[0].prod()], placement=Tile(3, 4))
+    worker_relu_col3_w1 = Worker(core_fn=corefunc2, fn_args=[externalfunc2, L1_L1_add_to_relu_8.cons(), MEM_L1_L2_D7D8_col3[1].prod()], placement=Tile(3, 2))
 
-    Workers = [A1_B1_worker, A2_B2_worker, A3_B3_worker, A4_B4_worker, A5_B5_worker, A6_B6_worker, A7_B7_worker, A8_B8_worker, C1_worker, C2_worker, C3_worker, C4_worker, C5_worker, C6_worker, C7_worker, C8_worker]
+    Workers = [worker_add_col0_w0, worker_add_col0_w1, worker_add_col1_w0, worker_add_col1_w1, worker_add_col2_w0, worker_add_col2_w1, worker_add_col3_w0, worker_add_col3_w1, worker_relu_col0_w0, worker_relu_col0_w1, worker_relu_col1_w0, worker_relu_col1_w1, worker_relu_col2_w0, worker_relu_col2_w1, worker_relu_col3_w0, worker_relu_col3_w1]
 
     # Runtime operations to move data to/from the AIE-array
     rt = Runtime()
-    with rt.sequence(chunk_a, chunk_b, chunk_d) as (A, B, D):
+    with rt.sequence(chunk_ty, chunk_ty, chunk_ty) as (A, B, D):
         rt.start(*Workers)
         rt.fill(placement=Tile(0, 0), in_fifo=SHIM_L3_L2_A1A2_col0.prod(), source=A, tap=TensorAccessPattern(tensor_dims=[inputA.numel()], offset=((inputA.numel() // 4) * 0), sizes=[((inputA.numel() // 4) // (inputA.numel() // 8)), (inputA.numel() // 8)], strides=[(inputA.numel() // 8), 1]))
         rt.fill(placement=Tile(1, 0), in_fifo=SHIM_L3_L2_A3A4_col1.prod(), source=A, tap=TensorAccessPattern(tensor_dims=[inputA.numel()], offset=((inputA.numel() // 4) * 1), sizes=[((inputA.numel() // 4) // (inputA.numel() // 8)), (inputA.numel() // 8)], strides=[(inputA.numel() // 8), 1]))
@@ -141,24 +144,6 @@ def main():
     inputB = iron.arange(data_size, dtype=datatype, device="npu")
     outputD = iron.zeros(data_size, dtype=datatype, device="npu")
     base_aaa(inputA, inputB, outputD)
-    print(outputD)
-    inputA_data = np.arange(data_size, dtype=np.float32)
-    inputB_data = np.arange(data_size, dtype=np.float32)
-    expected = np.maximum(0, (inputA_data + inputB_data))
-    actual = np.asarray(outputD, dtype=np.float32)
-    print('Element-by-element comparison:')
-    for i in range(data_size):
-        print(f'Expected = ReLU({inputA_data[i]:.1f} + {inputB_data[i]:.1f}) = {expected[i]:.1f} : Received = {actual[i]:.1f}')
-    tolerance = 1e-3
-    mismatches = np.where(~np.isclose(actual, expected, rtol=tolerance))[0]
-    if len(mismatches) == 0:
-        print(f"'Validation passed: Output matches expected for all 128 elements'")
-    else:
-        print(f'Validation failed: {len(mismatches)} mismatches found')
-        for idx in mismatches[:5]:
-            print(f'Index {idx}: actual={actual[idx]:.1f}, expected={expected[idx]:.1f}')
-        if len(mismatches) > 5:
-            print(f'... and {len(mismatches) - 5} more mismatches')
 
 
 
--- aiecad/aiecad_compiler/main.py
+++ aiecad/aiecad_compiler/main.py
@@ -4,6 +4,7 @@ main.py — End-to-end XML to Python code generation
 
 This is the main entry point for the IRON code generation system.
 It takes an XML file as input and generates Python code by:
+0. Expanding GUI-friendly XML to complete XML (XMLGenerator) if needed
 1. Converting XML to semantic graph (GraphDriver)
 2. Generating Python code from graph (CodeGenerator)
 3. Optionally executes the generated code (with --run flag)
@@ -12,10 +13,12 @@ Usage:
     python main.py <xml_file> [--run]
 
 Examples:
-    python main.py examples/passthrough/passthrough.xml           # Generate code only
+    python main.py examples/passthrough2/passthrough_gui.xml      # Generate from GUI XML
+    python main.py examples/passthrough/passthrough.xml           # Generate from complete XML
     python main.py examples/passthrough/passthrough.xml --run     # Generate and execute code
 
 Output:
+    - <xml_dir>/<name>_complete.xml: Complete XML (if GUI XML was input)
     - <xml_dir>/<name>.graphml: Semantic graph representation
     - <xml_dir>/generated_<name>.py: Generated Python code
 """
@@ -29,15 +32,22 @@ sys.path.insert(0, str(project_root))
 
 from codegen.backends.CodeGenerator import CodeGenerator
 from graph_builder.GraphDriver import GraphBuilder
+from graph_builder.XMLGenerator import XMLTransformer
+
+
+def _is_gui_xml(xml_path: Path) -> bool:
+    """Detect if XML is GUI-friendly format by checking filename."""
+    return '_gui' in xml_path.name
 
 
 def main():
     """
     Main entry point for XML to Python code generation.
-    
+
     Process:
+    0. Detect if input is GUI XML and expand it to complete XML (XMLGenerator)
     1. Validate command-line arguments
-    2. Build semantic graph from XML (GraphDriver)
+    2. Build semantic graph from complete XML (GraphDriver)
     3. Generate Python code from graph (CodeGenerator)
     4. Optionally execute generated code
     5. Report results
@@ -47,50 +57,81 @@ def main():
         print(f"Usage: {sys.argv[0]} <xml_file> [--run]")
         print()
         print("Arguments:")
-        print("  <xml_file>  Path to XML input file")
+        print("  <xml_file>  Path to XML input file (GUI or complete)")
         print("  --run       Execute generated code after creation (optional)")
         print()
         print("Examples:")
-        print(f"  {sys.argv[0]} examples/passthrough/passthrough.xml           # Generate code only")
+        print(f"  {sys.argv[0]} examples/passthrough2/passthrough_gui.xml      # GUI XML workflow")
+        print(f"  {sys.argv[0]} examples/passthrough/passthrough.xml           # Complete XML workflow")
         print(f"  {sys.argv[0]} examples/passthrough/passthrough.xml --run     # Generate and execute")
         print()
         print("Output files are created in the same directory as the input XML:")
+        print("  - <name>_complete.xml (if GUI XML was used)")
         print("  - <name>.graphml (semantic graph)")
         print("  - generated_<name>.py (Python code)")
         sys.exit(1)
-    
+
     xml_path = Path(sys.argv[1])
     run_after = len(sys.argv) == 3 and sys.argv[2] == '--run'
-    
+
     # Validate --run flag if provided
     if len(sys.argv) == 3 and sys.argv[2] != '--run':
         print(f"Error: Unknown flag '{sys.argv[2]}'")
         print(f"Usage: {sys.argv[0]} <xml_file> [--run]")
         sys.exit(1)
-    
+
     # Validate XML file exists
     if not xml_path.is_file():
         print(f"Error: File not found: {xml_path}")
         sys.exit(1)
-    
+
     if not xml_path.suffix == '.xml':
         print(f"Warning: File does not have .xml extension: {xml_path}")
-    
+
     print("=" * 70)
     print("IRON Code Generation System")
     print("=" * 70)
     print()
-    
+
+    # Step 0: Detect and expand GUI XML if needed
+    working_xml_path = xml_path
+    if _is_gui_xml(xml_path):
+        print(f"[0/3] Detected GUI XML - Expanding to complete XML...")
+        print(f"      Input: {xml_path}")
+
+        try:
+            # Generate complete XML path
+            complete_xml_path = xml_path.parent / xml_path.name.replace('_gui.xml', '_complete.xml')
+            if complete_xml_path == xml_path:
+                # If name doesn't have _gui, add _complete before .xml
+                complete_xml_path = xml_path.with_suffix('') / f"{xml_path.stem}_complete.xml"
+                complete_xml_path = xml_path.parent / f"{xml_path.stem}_complete.xml"
+
+            # Transform GUI XML to complete XML
+            transformer = XMLTransformer(xml_path)
+            transformer.save(complete_xml_path)
+
+            working_xml_path = complete_xml_path
+            print(f"      Output: {complete_xml_path}")
+            print()
+
+        except Exception as e:
+            print(f"      ERROR: Failed to expand GUI XML")
+            print(f"      {type(e).__name__}: {e}")
+            sys.exit(1)
+
     # Step 1: Build semantic graph from XML
-    print(f"[1/2] Building semantic graph from XML...")
-    print(f"      Input: {xml_path}")
-    
+    print(f"[1/3] Building semantic graph from XML...")
+    print(f"      Input: {working_xml_path}")
+
     try:
-        builder = GraphBuilder(xml_path)
+        builder = GraphBuilder(working_xml_path)
         graph = builder.build()
         
         # Save graph to GraphML in same directory as XML
-        graphml_path = xml_path.with_suffix('.graphml')
+        # Strip _gui suffix if present for output filenames
+        base_name = xml_path.stem.replace('_gui', '')
+        graphml_path = xml_path.parent / f"{base_name}.graphml"
         import networkx as nx
         nx.write_graphml(graph, graphml_path)
         
@@ -112,7 +153,7 @@ def main():
         sys.exit(1)
     
     # Step 2: Generate Python code from graph
-    print(f"[2/2] Generating Python code from graph...")
+    print(f"[2/3] Generating Python code from graph...")
     print(f"      Input: {graphml_path}")
     
     try:
@@ -120,8 +161,9 @@ def main():
         code = generator.generate()
         
         # Save generated code in same directory as XML
-        output_path = xml_path.with_suffix('.py')
-        output_path = output_path.parent / f"generated_{output_path.name}"
+        # Strip _gui suffix if present for output filenames
+        base_name = xml_path.stem.replace('_gui', '')
+        output_path = xml_path.parent / f"generated_{base_name}.py"
         
         with open(output_path, 'w') as f:
             f.write(code)
